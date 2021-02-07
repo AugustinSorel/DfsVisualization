4d25457ec8eb9ae2aa4b04cd858d36206ddac378
tartie sorel <sorelaugustin@gmail.com>
2021-02-07 18:28:16 +0000

    Maze User Control added

---
+++ DfsVisualization/MazeUserControl/MazeUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DfsVisualization
+{
+    /// <summary>
+    /// Interaction logic for MazeUserControl.xaml
+    /// </summary>
+    public partial class MazeUserControl : UserControl
+    {
+        public MazeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
