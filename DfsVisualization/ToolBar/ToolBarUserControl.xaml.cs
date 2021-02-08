847d7787bbbe51fa98f9547c851702e336d36f72
tartie sorel <sorelaugustin@gmail.com>
2021-02-08 21:55:14 +0000

    slider style added

--- DfsVisualization/ToolBar/ToolBarUserControl.xaml.cs
+++ DfsVisualization/ToolBar/ToolBarUserControl.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DfsVisualization
 {
     /// <summary>
     /// Interaction logic for ToolBarUserControl.xaml
     /// </summary>
-    public partial class ToolBarUserControl : UserControl
+    public partial class ToolBarUserControl : UserControl, INotifyPropertyChanged
     {
         public ToolBarUserControl()
         {
+            DataContext = this;
             InitializeComponent();
         }
+
+        private int _boundNumber = 5;
+
+        public int BoundNumber
+        {
+            get { return _boundNumber; }
+            set 
+            {
+                if (_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
