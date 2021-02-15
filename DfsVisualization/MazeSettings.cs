db2700fc0ad7a5fc970096bd75582187d18f4c7f
tartie sorel <sorelaugustin@gmail.com>
2021-02-15 20:58:26 +0000

    maze settings class added

---
+++ DfsVisualization/MazeSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace DfsVisualization
+{
+    class MazeSettings
+    {
+        #region Fields
+        private int startCellY;
+        private int startCellX;
+        #endregion
+
+        #region Properties
+        public int StartCellY
+        {
+            get { return startCellY; }
+            set { startCellY = value; }
+        }
+
+        public int StartCellx
+        {
+            get { return startCellX; }
+            set { startCellX = value; }
+        }
+        #endregion
+
+        public MazeSettings()
+        {
+            startCellX = 0;
+            startCellY = 0;
+        }
+    }
+}
