6dac95995a9e59e9106125b07c13c29fed35cc8d
tartie sorel <sorelaugustin@gmail.com>
2021-02-16 21:10:08 +0000

    output message class created

--- DfsVisualization/SettingsUserControl/SettingsUserControl.xaml.cs
+++ DfsVisualization/SettingsUserControl/SettingsUserControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace DfsVisualization
@@ -12,6 +11,13 @@ namespace DfsVisualization
         private readonly OpacityFade opacityFade;
         private readonly MazeSettings mazeSettings;
 
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; }
+        }
+
         public SettingsUserControl(MazeSettings mazeSettings)
         {
             InitializeComponent();
@@ -19,6 +25,7 @@ namespace DfsVisualization
             opacityFade = new OpacityFade(this);
             opacityFade.StartAnimation(true);
             SetControlsValue();
+            DataContext = this;
         }
 
         private void SetControlsValue()
@@ -39,13 +46,45 @@ namespace DfsVisualization
         {
             mazeSettings.NumberOfCellsX = (int)(Application.Current.Windows[0] as MainWindow).container.ColumnDefinitions[1].ActualWidth / mazeSettings.CellWidth;
             mazeSettings.NumberOfCellsY = (int)(Application.Current.Windows[0] as MainWindow).container.RowDefinitions[2].ActualHeight / mazeSettings.CellHeight;
-            
-            mazeSettings.StartCellx = int.TryParse(TextBoxStartCellX.Text, out int startCellX) ? startCellX : 0;
-            mazeSettings.StartCellY = int.TryParse(TextBoxStartCellY.Text, out int startCellY) ? startCellY : 0;
 
-            mazeSettings.CellWidth = int.TryParse(textboxCellWidth.Text, out int cellWidth) ? cellWidth : 20;
-            mazeSettings.CellHeight = int.TryParse(textboxCellHeight.Text, out int cellHeight) ? cellHeight : 20;
+            string errorMessage = string.Empty;
 
+            if (!int.TryParse(TextBoxStartCellX.Text, out int startCellX))
+            {
+                mazeSettings.StartCellx = 0;
+                errorMessage += "StartCellX ";
+            }
+            else
+                mazeSettings.StartCellx = startCellX;
+
+            if (!int.TryParse(TextBoxStartCellY.Text, out int startCellY))
+            {
+                mazeSettings.StartCellY = 0;
+                errorMessage += "StartCellY ";
+            }
+            else
+                mazeSettings.StartCellY = startCellY;
+
+            if (!int.TryParse(textboxCellWidth.Text, out int cellWidth))
+            {
+                mazeSettings.CellWidth = 20;
+                errorMessage += "CellWidth ";
+            }
+            else
+                mazeSettings.CellWidth = cellWidth;
+
+            if (!int.TryParse(textboxCellHeight.Text, out int cellHeight))
+            {
+                mazeSettings.CellHeight = 20;
+                errorMessage += "CellHeight ";
+            }
+            else
+                mazeSettings.CellHeight = cellHeight;
+
+            if (errorMessage != string.Empty)
+                errorMessage = "Cannot convert " + errorMessage;
+
+            ErrorMessage = errorMessage;
             SetControlsValue();
         }
     }
