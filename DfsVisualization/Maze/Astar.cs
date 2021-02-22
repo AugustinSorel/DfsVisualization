fbccb10f4656cfe828005af90149511481a04cf1
tartie sorel <sorelaugustin@gmail.com>
2021-02-22 20:51:58 +0000

    shortest path test

--- DfsVisualization/Maze/Astar.cs
+++ DfsVisualization/Maze/Astar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Media;
 
@@ -37,21 +38,84 @@ namespace DfsVisualization
             Application.Current.Dispatcher.Invoke(new Action(() => { mazeDrawer.Cells[mazeSettings.AStartEndX, mazeSettings.AStartEndY].Background = GlobalColors.TargerCellColor; }));
         }
 
-
+        bool[,] wasHere;
+        bool[,] correctPath; // The solution to the maze
+        int startX, startY; // Starting X and Y values of maze
+        int endX, endY; 
 
         internal void Start()
         {
             CreateMaze();
-            return;
-            //for (int i = 0; i < maze.GetLength(1); i++)
-            //{
-            //    for (int j = 0; j < maze.GetLength(0); j++)
-            //    {
-            //        MessageBox.Show(maze[j, i].ToString());
-            //    }
-            //    MessageBox.Show("** End j **");
-            //}
 
+            wasHere = new bool[maze.GetLength(1),maze.GetLength(1)];
+            correctPath = new bool[maze.GetLength(1),maze.GetLength(1)];
+
+            startX = 0;
+            endX = 4;
+
+            startY = 0;
+            endY = 4;
+
+            for (int row = 0; row < maze.GetLength(1); row++)
+                // Sets boolean Arrays to default values
+                for (int col = 0; col < maze.GetLength(0); col++)
+                {
+                    wasHere[row, col] = false;
+                    correctPath[row, col] = false;
+                }
+            bool b = recursiveSolve(startX, startY);
+
+
+
+            using (var sw = new StreamWriter("Solution.txt"))
+            {
+                for (int i = 0; i < Maze.GetLength(1); i++)
+                {
+                    for (int j = 0; j < Maze.GetLength(0); j++)
+                    {
+                        string temp = correctPath[j, i] ? "X" : "O";
+                        sw.Write(temp + " ");
+                    }
+                    sw.Write("\n");
+                }
+
+                sw.Flush();
+                sw.Close();
+            }
+
+        }
+
+        private bool recursiveSolve(int x, int y)
+        {
+            if (x == endX && y == endY) return true; // If you reached the end
+            if (maze[x, y] || wasHere[x, y]) return false;
+            // If you are on a wall or already were here
+            wasHere[x, y] = true;
+            if (x != 0) // Checks if not on left edge
+                if (recursiveSolve(x - 1, y))
+                { // Recalls method one to the left
+                    correctPath[x, y] = true; // Sets that path value to true;
+                    return true;
+                }
+            if (x != maze.GetLength(0) - 1) // Checks if not on right edge
+                if (recursiveSolve(x + 1, y))
+                { // Recalls method one to the right
+                    correctPath[x, y] = true;
+                    return true;
+                }
+            if (y != 0)  // Checks if not on top edge
+                if (recursiveSolve(x, y - 1))
+                { // Recalls method one up
+                    correctPath[x, y] = true;
+                    return true;
+                }
+            if (y != maze.GetLength(1) - 1) // Checks if not on bottom edge
+                if (recursiveSolve(x, y + 1))
+                { // Recalls method one down
+                    correctPath[x, y] = true;
+                    return true;
+                }
+            return false;
         }
 
         private void CreateMaze()
